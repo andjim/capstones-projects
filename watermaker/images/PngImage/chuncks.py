3caa4e1507d09d3589504ccc7105dcf4a94575bb
Anderson Jimenez Santana <andjim2598@gmail.com>
2021-07-23 12:24:55 -0400

    [UPD] - add critical chuck classes inheriting from basechunck

--- watermaker/images/PngImage/chuncks.py
+++ watermaker/images/PngImage/chuncks.py
@@ -35,3 +35,71 @@ class PngChunck:
             self.int_lenght, self.type_as_str, self.crc
         )
 
+class PngIHDR(PngChunck):
+    IHDR_FIELDS = {
+        'width': 4, 'height': 4,
+        'bit_depth': 1, 'color_type': 1,
+        'compression_method': 1, 'filter_method': 1,
+        'interlace_method': 1
+    }
+    def __init__(self, lenght: bytes, chunck_type: bytes, data: bytes, crc: bytes) -> None:
+        super().__init__(lenght, chunck_type, data, crc)
+        cursor = 0
+        for field, size in self.IHDR_FIELDS.items():
+            setattr(self,'_'+field, self.data[cursor: cursor+size])
+            cursor += size
+
+    @property
+    def width(self):
+        return int.from_bytes(self._width, byteorder='big')
+    
+    @property
+    def height(self):
+        return int.from_bytes(self._height, byteorder='big')
+
+    @property
+    def bit_depth(self):
+        return int.from_bytes(self._bit_depth, byteorder='big')
+    
+    @property
+    def color_type(self):
+        return int.from_bytes(self._color_type, byteorder='big')
+
+    @property
+    def compression_method(self):
+        return int.from_bytes(self._compression_method, byteorder='big')
+    
+    @property
+    def filter_method(self):
+        return int.from_bytes(self._filter_method, byteorder='big')
+
+    @property
+    def interlace_method(self):
+        return int.from_bytes(self._interlace_method, byteorder='big')
+
+    def __repr__(self) -> str:
+        return super().__repr__().replace('PngChunck', 'PngIHDRChunck')
+
+
+class PngPLTE(PngChunck):
+    def __init__(self, lenght: bytes, chunck_type: bytes, data: bytes, crc: bytes) -> None:
+        super().__init__(lenght, chunck_type, data, crc)
+    
+    def __repr__(self) -> str:
+        return super().__repr__().replace('PngChunck', 'PngPLTEChunck')
+
+
+class PngIDAT(PngChunck):
+    def __init__(self, lenght: bytes, chunck_type: bytes, data: bytes, crc: bytes) -> None:
+        super().__init__(lenght, chunck_type, data, crc)
+    
+    def __repr__(self) -> str:
+        return super().__repr__().replace('PngChunck', 'PngIDATChunck')
+
+
+class PngIEND(PngChunck):
+    def __init__(self, lenght: bytes, chunck_type: bytes, data: bytes, crc: bytes) -> None:
+        super().__init__(lenght, chunck_type, data, crc)
+    
+    def __repr__(self) -> str:
+        return super().__repr__().replace('PngChunck', 'PngIENDChunck')
